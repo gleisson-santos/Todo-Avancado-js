58f28e0cc514924bda35ef4ac12c5f50694c7b45
Glfx20 <jsgleisson@gmail.com>
2022-12-07 17:08:46 -0300

    commit
    Atualizações dos Botoes

--- Todo/js/scripts.js
+++ Todo/js/scripts.js
@@ -17,7 +17,7 @@ const saveTodo = (text) => {
     todoTitle.innerText = text;
     todo.appendChild(todoTitle);
 
-    //Botões de Edição
+    //Botões de Edição / Criar parte do HTML  em js
     const doneBtn = document.createElement("button");
     doneBtn.classList.add("finish-todo");
     doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
@@ -36,6 +36,11 @@ const saveTodo = (text) => {
 
     //Adicionar todo o todo a lista  da Div
     todoList.appendChild(todo);
+
+    //Deixa input vazio apos enviar o texto
+    todoInput.value = "";
+    //Focar na caixa do input apos cadastrar
+    todoInput.focus();
 };
 
 
@@ -51,4 +56,29 @@ todoForm.addEventListener("submit", (e) => {
         // Salvar o todo
         saveTodo(inputValue);
     }
+});
+
+
+document.addEventListener("click", (e) => {
+
+    const targetEl = e.target;
+    const parentEl = targetEl.closest("div");
+
+    //Completrar tarefas
+    if (targetEl.classList.contains("finish-todo")) {
+        parentEl.classList.toggle("done")
+    }
+    
+    //Exluir tarefas
+    if(targetEl.classList.contains("remove-todo")) {
+        parentEl.remove();
+    }
+
+    //Exluir tarefas
+    if(targetEl.classList.contains("edit-todo")) {
+        console.log("Editou")
+    }
+
+
+
 });
