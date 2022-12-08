835a7e8a4db3819e122cbc555aaf71f7f24fd8a8
Glfx20 <jsgleisson@gmail.com>
2022-12-08 12:47:57 -0300

    commit

--- Todo/js/scripts.js
+++ Todo/js/scripts.js
@@ -1,136 +1,135 @@
 // Seleção de elementos
-const todoForm = document.querySelector("#todo-form");
-const todoInput = document.querySelector("#todo-input");
-const todoList = document.querySelector("#todo-list");
-const editForm = document.querySelector("#edit-form");
-        const editInput = document.querySelector("#edit-input");
-const cancelEditiBtn = document.querySelector("#cancel-edit-btn");
+const todoForm = document.querySelector('#todo-form')
+const todoInput = document.querySelector('#todo-input')
+const todoList = document.querySelector('#todo-list')
+const editForm = document.querySelector('#edit-form')
+const editInput = document.querySelector('#edit-input')
+const cancelEditiBtn = document.querySelector('#cancel-edit-btn')
 
 //Salvar na memoria para edição
-let oldInputValue;
+let oldInputValue
 
 //Funções
 const saveTodo = (text) => {
+  //Criação da div com uma classe
+  const todo = document.createElement('div')
+  todo.classList.add('todo')
 
-    //Criação da div com uma classe
-    const todo = document.createElement("div");
-    todo.classList.add("todo");
+  const todoTitle = document.createElement('h3')
+  todoTitle.innerText = text
+  todo.appendChild(todoTitle)
 
-    const todoTitle = document.createElement("h3");
-    todoTitle.innerText = text;
-    todo.appendChild(todoTitle);
+  //Botões de Edição / Criar parte do HTML  em js
+  const doneBtn = document.createElement('button')
+  doneBtn.classList.add('finish-todo')
+  doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+  todo.appendChild(doneBtn)
 
-    //Botões de Edição / Criar parte do HTML  em js
-    const doneBtn = document.createElement("button");
-    doneBtn.classList.add("finish-todo");
-    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
-    todo.appendChild(doneBtn);
+  const editBtn = document.createElement('button')
+  editBtn.classList.add('edit-todo')
+  editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
+  todo.appendChild(editBtn)
 
-    const editBtn = document.createElement("button");
-    editBtn.classList.add("edit-todo");
-    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
-    todo.appendChild(editBtn);
+  const deletBtn = document.createElement('button')
+  deletBtn.classList.add('remove-todo')
+  deletBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+  todo.appendChild(deletBtn)
 
-    const deletBtn = document.createElement("button");
-    deletBtn.classList.add("remove-todo");
-    deletBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
-    todo.appendChild(deletBtn);
-    
+  //Adicionar todo o todo a lista  da Div
+  todoList.appendChild(todo)
 
-    //Adicionar todo o todo a lista  da Div
-    todoList.appendChild(todo);
-
-    //Deixa input vazio apos enviar o texto
-    todoInput.value = "";
-    //Focar na caixa do input apos cadastrar
-    todoInput.focus();
-};
+  //Deixa input vazio apos enviar o texto
+  todoInput.value = ''
+  //Focar na caixa do input apos cadastrar
+  todoInput.focus()
+}
 
 //Esconder itens para a edição
 const toggleForms = () => {
-    editForm.classList.toggle("hide");
-    todoForm.classList.toggle("hide");
-    todoList.classList.toggle("hide");
-};
+  editForm.classList.toggle('hide')
+  todoForm.classList.toggle('hide')
+  todoList.classList.toggle('hide')
+}
 
 const updateTodo = (text) => {
-    const todos = document.querySelectorAll(".todo")  ;
-    
-    todos.forEach((todo) => {
-        let todoTitle = todo.querySelector("h3");
+  const todos = document.querySelectorAll('.todo')
 
-        if(todoTitle.innerText === oldInputValue) {
-            todoTitle.innerText = text;
-        }
-    });
-};
+  todos.forEach((todo) => {
+    let todoTitle = todo.querySelector('h3')
+
+    if (todoTitle.innerText === oldInputValue) {
+      todoTitle.innerText = text
+    }
+  })
+}
 
 //Eventos
 
-todoForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+todoForm.addEventListener('submit', (e) => {
+  e.preventDefault()
 
-    const inputValue = todoInput.value;
+  const inputValue = todoInput.value
 
-    if (inputValue) {
-        console.log(inputValue);
-        // Salvar o todo
-        saveTodo(inputValue);
-    }
-});
+  if (inputValue) {
+    console.log(inputValue)
+    // Salvar o todo
+    saveTodo(inputValue)
+  }
+})
 
+document.addEventListener('click', (e) => {
+  const targetEl = e.target
+  //Selecionar o elemento pai Div mais proximo
+  const parentEl = targetEl.closest('div')
+  let todoTitle
 
-document.addEventListener("click", (e) => {
+  //Salvar na memoria para edição
+  if (parentEl && parentEl.querySelector('h3')) {
+    todoTitle = parentEl.querySelector('h3').innerText
+  }
 
-    const targetEl = e.target;
-    //Selecionar o elemento pai Div mais proximo
-    const parentEl = targetEl.closest("div");
-    let todoTitle;
+  //Completrar tarefas
+  if (targetEl.classList.contains('finish-todo')) {
+    //toggle serve para habilitar e/ou desabilitar a função
+    parentEl.classList.toggle('done')
+  }
+
+  //Remove tarefas
+  if (targetEl.classList.contains('remove-todo')) {
+    parentEl.remove()
+  }
+
+  //Editar tarefas
+  if (targetEl.classList.contains('edit-todo')) {
+    toggleForms()
 
     //Salvar na memoria para edição
-    if (parentEl && parentEl.querySelector("h3")) {
-        todoTitle = parentEl.querySelector("h3").innerText;
-    }
+    editInput.value = todoTitle
+    oldInputValue = todoTitle
+  }
+})
 
-    //Completrar tarefas
-    if (targetEl.classList.contains("finish-todo")) {
-        //toggle serve para habilitar e/ou desabilitar a função
-        parentEl.classList.toggle("done");
-    }
-    
-    //Remove tarefas
-    if(targetEl.classList.contains("remove-todo")) {
-        parentEl.remove();
-    }
+cancelEditiBtn.addEventListener('click', (e) => {
+  //preventDefault par anão enviar formulario ao clicar no botão
+  e.preventDefault()
 
-    //Editar tarefas
-    if(targetEl.classList.contains("edit-todo")) {
-        toggleForms();
+  toggleForms()
+})
 
-        //Salvar na memoria para edição
-        editInput.value =  todoTitle;
-        oldInputValue = todoTitle;
-    }
-});
+editForm.addEventListener('submit', (e) => {
+  e.preventDefault()
 
-cancelEditiBtn.addEventListener("click", (e) => {
-    //preventDefault par anão enviar formulario ao clicar no botão
-    e.preventDefault();
+  const editInputValue = editInput.value
 
-    toggleForms();
-});
+  if (editInputValue) {
+    // atualizar
+    updateTodo(editInputValue)
+  }
 
-editForm.addEventListener("submit", (e) => {
+  toggleForms()
+})
 
-    e.preventDefault();
 
-    const editInputValue = editInput.value
-
-    if(editInputValue) {
-        // atualizar
-        updateTodo(editInputValue)
-    }
-
-    toggleForms();
-
-});
+editForm.addEventListener("Submit", (e) => {
+  
+})
